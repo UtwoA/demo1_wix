631ff007908c4294d352a0da1a587b99abe9736e
Алексей <workspace.alexey@gmail.com>
2024-07-30 19:03:45 +0400

    colored form

--- src/pages/Страница корзины1.hiys7.js
+++ src/pages/Страница корзины1.hiys7.js
@@ -51,6 +51,7 @@ $w.onReady(function () {
             // Скрыть первую форму и показать вторую
             $w('#form15').show();
             $w('#submitButton').hide();
+            $w('#form15').style.backgroundColor='000000';
         } else {
             // Если поля пустые, можно показать сообщение об ошибке
             console.log("Пожалуйста, заполните все поля.");
