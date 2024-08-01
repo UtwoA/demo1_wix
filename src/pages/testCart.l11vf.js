521d8a3d5c2dbd12f9f74612418449eb0dbf5659
Алексей <workspace.alexey@gmail.com>
2024-08-01 16:13:05 +0400

    easy mode

--- src/pages/testCart.l11vf.js
+++ src/pages/testCart.l11vf.js
@@ -25,45 +25,8 @@ $w.onReady(function () {
     $w('#repeater1').onItemReady(($item, itemData) => {
         console.log('Item Data in onItemReady:', itemData); // Отладка: вывод данных элемента Repeater
 
-        if (itemData.mediaItem) {
-            //$item('#itemImage').src = itemData.mediaItem.src; // Установите источник изображения
-        } else {
-            console.log('No mediaItem found');
-        }
-
         $item('#itemTitle').text = itemData.name || ''; // Установите название товара
-       // $item('#itemPrice').text = `$${itemData.price || 0}`; // Установите цену товара
-
-        // Обработка удаления товара из корзины
-        $item('#removeButton').onClick(() => {
-            wixStores.cart.removeProducts([itemData.productId])
-                .then(() => {
-                    console.log('Товар удален из корзины');
-                    updateCart(); // Обновление содержимого корзины
-                })
-                .catch((err) => {
-                    console.log('Ошибка при удалении товара из корзины:', err);
-                });
-        });
     });
-
-    // Обновление интерфейса после изменения корзины
-    function updateCart() {
-        wixStores.cart.getCurrentCart()
-            .then((cart) => {
-                const cartItems = cart.lineItems;
-                console.log('Updated Cart Items:', cartItems); // Отладка: вывод обновленных данных корзины
-
-                if (cartItems.length > 0) {
-                    $w('#repeater1').data = cartItems;
-                } else {
-                    $w('#repeater1').collapse(); // Скрыть Repeater, если корзина пуста
-                }
-            })
-            .catch((err) => {
-                console.log('Ошибка при обновлении интерфейса корзины:', err);
-            });
-    }
 });
 
 // Функция для оформления заказа
