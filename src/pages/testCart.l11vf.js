893cd6b580365b23de2222d3a8471a2c3f369eda
Алексей <workspace.alexey@gmail.com>
2024-08-01 17:23:47 +0400

    structure

--- src/pages/testCart.l11vf.js
+++ src/pages/testCart.l11vf.js
@@ -1,33 +1,32 @@
 import wixStores from 'wix-stores';
 
 $w.onReady(function () {
-    // Получение содержимого корзины
     wixStores.cart.getCurrentCart()
         .then((cart) => {
             const cartItems = cart.lineItems;
             console.log('Cart Items:', cartItems);
 
             if (Array.isArray(cartItems) && cartItems.length > 0) {
-                // Преобразование данных, если это необходимо
                 const formattedItems = cartItems.map(item => ({
-                    _id: item.id || item._id, // Проверьте правильный id
+                    _id: item.id || item._id,
                     name: item.name || 'No Name',
                     mediaItem: item.mediaItem || { src: '' },
                     price: item.price || 0,
                     quantity: item.quantity || 1,
-                    totalPrice: item.totalPrice || 0,
-                    // Добавьте другие поля по необходимости
+                    totalPrice: item.totalPrice || 0
                 }));
 
                 // Установка данных в Repeater
                 $w('#repeater1').data = formattedItems;
-                console.log('Formatted Data set to repeater:', formattedItems); // Проверка данных перед установкой
-                console.log('Data in $w("#repeater1").data:', $w('#repeater1').data);
-                // Используем функцию для обновления элементов Repeater
-                updateRepeaterItems();
+                console.log('Formatted Data set to repeater:', formattedItems);
+
+                // Обновление элементов Repeater
+                $w('#repeater1').onItemReady(() => {
+                    updateRepeaterItems();
+                });
             } else {
                 console.log('Cart is empty or cartItems is not an array');
-                $w('#repeater1').collapse(); // Скрыть Repeater, если корзина пуста
+                $w('#repeater1').collapse();
             }
         })
         .catch((err) => {
@@ -35,24 +34,22 @@ $w.onReady(function () {
         });
 });
 
-// Функция для обновления элементов Repeater
 function updateRepeaterItems() {
-    // Обновление элементов внутри Repeater
-    $w('#repeater1').forEachItem(($item, itemData, index) => {
+    $w('#repeater1').forEachItem(($item, itemData) => {
         console.log('Item Data in forEachItem:', itemData);
 
-        // Убедитесь, что itemData содержит правильные данные
         if (itemData) {
             $item('#itemTitle').text = itemData.name || 'No Name';
             
             if (itemData.mediaItem && itemData.mediaItem.src) {
                 $item('#itemImage').src = itemData.mediaItem.src;
             } else {
-                $item('#itemImage').src = ''; // Путь по умолчанию, если нет изображения
+                $item('#itemImage').src = '';
             }
 
             $item('#itemPrice').text = `$${itemData.price || 0}`;
-
+            
+            // Пример кнопки удаления
             if ($item('#removeButton')) {
                 $item('#removeButton').onClick(() => {
                     wixStores.cart.removeProducts([itemData._id])
@@ -71,7 +68,6 @@ function updateRepeaterItems() {
     });
 }
 
-// Функция для обновления интерфейса после изменения корзины
 function updateCart() {
     wixStores.cart.getCurrentCart()
         .then((cart) => {
@@ -79,20 +75,18 @@ function updateCart() {
             console.log('Updated Cart Items:', cartItems);
 
             if (Array.isArray(cartItems) && cartItems.length > 0) {
-                // Преобразование данных, если это необходимо
                 const formattedItems = cartItems.map(item => ({
-                    _id: item.id || item._id, // Проверьте правильный id
+                    _id: item.id || item._id,
                     name: item.name || 'No Name',
                     mediaItem: item.mediaItem || { src: '' },
                     price: item.price || 0,
                     quantity: item.quantity || 1,
-                    totalPrice: item.totalPrice || 0,
-                    // Добавьте другие поля по необходимости
+                    totalPrice: item.totalPrice || 0
                 }));
 
                 $w('#repeater1').data = formattedItems;
                 console.log('Formatted Data set to repeater after update:', formattedItems);
-                updateRepeaterItems(); // Обновление элементов Repeater
+                updateRepeaterItems();
             } else {
                 $w('#repeater1').collapse();
             }
