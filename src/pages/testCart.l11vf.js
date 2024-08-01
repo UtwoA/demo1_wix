c8858ea9b9b6ba29f0b1874706ea5064667f039b
Алексей <workspace.alexey@gmail.com>
2024-08-01 19:06:33 +0400

    asd

--- src/pages/testCart.l11vf.js
+++ src/pages/testCart.l11vf.js
@@ -19,27 +19,20 @@ $w.onReady(async function () {
 
         console.log('Formatted Products:', products);
 
-        // Вы можете использовать `products` как нужно, например, передать их в Repeater
-        // $w('#repeater1').data = products;
+        $w('#repeater1').data = products;
+  
+        $w('#repeater1').onItemReady(($item, itemData, index) => {
+          console.log(`Item data:`, itemData); // Вывод данных элемента в консоль
+      
+          $item('#itemTitle1').text = itemData.title;
+          $item('#itemPrice1').text = `$${itemData.price.toFixed(2)}`;
+          $item('#itemImage1').src = itemData.image;
+        });
 
     } catch (err) {
         console.log('Ошибка при получении содержимого корзины:', err);
     }
-    
-    const products = [
-      { _id: '1', title: 'Product 1', price: 29.99, image: 'https://example.com/image1.jpg' },
-      { _id: '2', title: 'Product 2', price: 39.99, image: 'https://example.com/image2.jpg' }
-    ];
-   console.log('products',products);
-    $w('#repeater1').data = products;
-  
-    $w('#repeater1').onItemReady(($item, itemData, index) => {
-      console.log(`Item data:`, itemData); // Вывод данных элемента в консоль
-  
-      $item('#itemTitle1').text = itemData.title;
-      $item('#itemPrice1').text = `$${itemData.price.toFixed(2)}`;
-      $item('#itemImage1').src = itemData.image;
-    });
+
   });
   
   
