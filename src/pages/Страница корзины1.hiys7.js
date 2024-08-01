860a0add00684d6b72d9205f4736a17558025ba5
Алексей <workspace.alexey@gmail.com>
2024-08-01 15:37:50 +0400

    return form

--- src/pages/Страница корзины1.hiys7.js
+++ src/pages/Страница корзины1.hiys7.js
@@ -1,68 +1,56 @@
 import wixStores from 'wix-stores';
 import wixLocation from 'wix-location';
 
-$w.onReady(function () {
-    // Получение содержимого корзины при загрузке страницы
-    wixStores.cart.getCurrentCart()
-        .then((cart) => {
-            const cartItems = cart.lineItems;
-            console.log('Cart Items:', cartItems); // Отладка: вывод данных корзины
-            if (cartItems.length > 0) {
-                $w('#repeater1').data = cartItems;
-            } else {
-                $w('#repeater1').collapse(); // Скрыть Repeater, если корзина пуста
-            }
-        })
-        .catch((err) => {
-            console.log('Ошибка при получении содержимого корзины:', err);
-        });
+$w.onReady(async function () {
 
-    // Обновление интерфейса после изменения корзины
-    function updateCart() {
-        wixStores.cart.getCurrentCart()
-            .then((cart) => {
-                const cartItems = cart.lineItems;
-                console.log('Updated Cart Items:', cartItems); // Отладка: вывод обновленных данных корзины
-                if (cartItems.length > 0) {
-                    $w('#repeater1').data = cartItems;
-                } else {
-                    $w('#repeater1').collapse(); // Скрыть Repeater, если корзина пуста
-                }
-            })
-            .catch((err) => {
-                console.log('Ошибка при обновлении интерфейса корзины:', err);
-            });
-    }
+    // Пример других обработчиков событий
+    $w('#buttonContactUs').onClick(() => {
+        $w('#footer1').scrollTo();
+    });
 
-    // Привязка данных к элементам Repeater
-    $w('#repeater1').onItemReady(($item, itemData) => {
-        console.log('Item Data in onItemReady:', itemData); // Отладка: вывод данных элемента Repeater
-        $item('#itemImage').src = itemData.mediaItem ? itemData.mediaItem.src : ''; // Установите источник изображения
-        $item('#itemTitle').text = itemData.name || ''; // Установите название товара
-        $item('#itemPrice').text = `$${itemData.price || 0}`; // Установите цену товара
+    $w('#closeSecondFormButton').onClick(() => {
+        $w('#submitButton').show();
+        $w('#form15').hide();
+    });
 
-        // Обработка удаления товара из корзины
-        $item('#removeButton').onClick(() => {
-            wixStores.cart.removeProducts([itemData.productId])
-                .then(() => {
-                    console.log('Товар удален из корзины');
-                    updateCart(); // Обновление содержимого корзины
-                })
-                .catch((err) => {
-                    console.log('Ошибка при удалении товара из корзины:', err);
-                });
-        });
+    $w('#checkoutButton').onClick(() => {
+        $w('#form14').scrollTo();
+    });
+
+    $w('#submitButton').onClick(async () => {
+        let userName = $w('#inputName').value;
+        let userEmail = $w('#inputEmail').value;
+        let userLastName = $w('#inputLastName').value;
+        let userPhone = $w('#inputPhone').value;
+        let userMessage = $w('#inputMessage').value;
+
+        let cart = await wixStores.cart.getCurrentCart();
+        let products = cart.lineItems.map(item => item.name).join(', ');
+
+        if (userPhone && userEmail && products) {
+            $w('#secondFormUserName').value = userName;
+            $w('#secondFormUserEmail').value = userEmail;
+            $w('#secondFormUserLastName').value = userLastName;
+            $w('#secondFormUserPhone').value = userPhone;
+            $w('#secondFormUserMessage').value = userMessage;
+            $w('#secondFormProductName').value = products;
+
+            $w('#form15').show();
+            $w('#submitButton').hide();
+        } else {
+            console.log("Пожалуйста, заполните все поля.");
+        }
+    });
+
+    $w('#button20').onClick(() => {
+        let userName = $w('#secondFormUserName').value;
+        let userLastName = $w('#secondFormUserLastName').value;
+        let userPhone = $w('#secondFormUserPhone').value;
+        let userEmail = $w('#secondFormUserEmail').value;
+        let userMessage = $w('#secondFormUserMessage').value;
+        let products = $w('#secondFormProductName').value;
+
+        console.log("Данные второй формы отправлены:", userName, userEmail);
     });
 });
 
-// Функция для оформления заказа
-export function checkout() {
-    wixStores.cart.checkout()
-        .then(() => {
-            console.log('Чек-аут успешен');
-            wixLocation.to('/checkout-success'); // Перенаправление на страницу подтверждения
-        })
-        .catch((err) => {
-            console.log('Ошибка при оформлении заказа:', err);
-        });
-}
