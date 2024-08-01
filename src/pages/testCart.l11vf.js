9759718100c3ef2bfd37a493d64c65564c966a1d
Алексей <workspace.alexey@gmail.com>
2024-08-01 18:21:43 +0400

    static

--- src/pages/testCart.l11vf.js
+++ src/pages/testCart.l11vf.js
@@ -1,29 +1,19 @@
 $w.onReady(function () {
-    // Данные, которые вы хотите отобразить
     const products = [
-      {
-        _id: '1',
-        title: 'Product 1',
-        price: 29.99,
-        image: 'https://example.com/image1.jpg'
-      },
-      {
-        _id: '2',
-        title: 'Product 2',
-        price: 39.99,
-        image: 'https://example.com/image2.jpg'
-      }
+      { _id: '1', title: 'Product 1', price: 29.99, image: 'https://example.com/image1.jpg' },
+      { _id: '2', title: 'Product 2', price: 39.99, image: 'https://example.com/image2.jpg' }
     ];
   
-    // Назначьте данные в Repeater
     $w('#repeater1').data = products;
   
-    // Обработка каждого элемента Repeater
     $w('#repeater1').onItemReady(($item, itemData, index) => {
-      // Присвойте значения элементам внутри Repeater
+      console.log(`Item data:`, itemData); // Вывод данных элемента в консоль
+  
       $item('#itemTitle1').text = itemData.title;
       $item('#itemPrice1').text = `$${itemData.price.toFixed(2)}`;
       $item('#itemImage1').src = itemData.image;
     });
   });
   
+  
+
