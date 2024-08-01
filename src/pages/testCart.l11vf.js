f99c31cc86d2d16f7969c80dd06a07b1ebfee85b
Алексей <workspace.alexey@gmail.com>
2024-08-01 16:17:10 +0400

    e

--- src/pages/testCart.l11vf.js
+++ src/pages/testCart.l11vf.js
@@ -22,8 +22,57 @@ $w.onReady(function () {
 
     // Привязка данных к элементам Repeater
     $w('#repeater1').onItemReady(($item, itemData) => {
+        console.log('onItemReady called'); // Отладка: подтверждение вызова onItemReady
         console.log('Item Data in onItemReady:', itemData); // Отладка: вывод данных элемента Repeater
 
-        $item('#itemTitle').text = itemData.name || ''; // Установите название товара
+        $item('#itemTitle').text = itemData.name || 'No Name'; // Установите название товара
+        // Добавьте дополнительную отладку для других элементов
+        if ($item('#itemImage')) {
+            console.log('Image element exists');
+            $item('#itemImage').src = itemData.mediaItem ? itemData.mediaItem.src : '';
+        } else {
+            console.log('Image element does not exist');
+        }
+        
+        if ($item('#itemPrice')) {
+            console.log('Price element exists');
+            $item('#itemPrice').text = `$${itemData.price || 0}`;
+        } else {
+            console.log('Price element does not exist');
+        }
+
+        if ($item('#removeButton')) {
+            console.log('Remove button exists');
+            $item('#removeButton').onClick(() => {
+                wixStores.cart.removeProducts([itemData.productId])
+                    .then(() => {
+                        console.log('Товар удален из корзины');
+                        updateCart(); // Обновление содержимого корзины
+                    })
+                    .catch((err) => {
+                        console.log('Ошибка при удалении товара из корзины:', err);
+                    });
+            });
+        } else {
+            console.log('Remove button does not exist');
+        }
     });
+
+    // Обновление интерфейса после изменения корзины
+    function updateCart() {
+        wixStores.cart.getCurrentCart()
+            .then((cart) => {
+                const cartItems = cart.lineItems;
+                console.log('Updated Cart Items:', cartItems); // Отладка: вывод обновленных данных корзины
+
+                if (cartItems.length > 0) {
+                    $w('#repeater1').data = cartItems;
+                } else {
+                    $w('#repeater1').collapse(); // Скрыть Repeater, если корзина пуста
+                }
+            })
+            .catch((err) => {
+                console.log('Ошибка при обновлении интерфейса корзины:', err);
+            });
+    }
 });
