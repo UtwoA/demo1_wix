56628e4890e10c878a50ff08cf67b71f2754f056
Алексей <workspace.alexey@gmail.com>
2024-08-03 01:23:48 +0400

    ывф

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,8 +1,11 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
-
 $w.onReady(function () {
-    $w('#buttonMap').onClick(() => {
-        $w('#googleMaps1').show();
+    $w("#googleMaps1").hide();
+
+    $w("#buttonMap").onClick(() => {
+        if ($w("#googleMaps1").isVisible) {
+            $w("#googleMaps1").hide();
+        } else {
+            $w("#googleMaps1").show();
+        }
     });
 });
