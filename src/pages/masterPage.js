3b68b168da73275173033285ca9eb26018a2e7ce
Алексей <workspace.alexey@gmail.com>
2024-08-03 01:24:53 +0400

    return to roman

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,11 +1,8 @@
-$w.onReady(function () {
-    $w("#googleMaps1").hide();
+// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
-    $w("#buttonMap").onClick(() => {
-        if ($w("#googleMaps1").isVisible) {
-            $w("#googleMaps1").hide();
-        } else {
-            $w("#googleMaps1").show();
-        }
+$w.onReady(function () {
+    $w('#buttonMap').onClick(() => {
+        $w('#googleMaps1').show();
     });
 });
