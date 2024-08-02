eb27c6a2e94529039d54ce5f26c2cc921f4aaaed
al26vin <savid662@gmail.com>
2024-08-02 23:51:38 +0300

    map

--- src/pages/testCart.l11vf.js
+++ src/pages/testCart.l11vf.js
@@ -2,7 +2,9 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    $w('#buttonCart').onClick(() => {
-        $w('#googleMaps1').show();
-    });
+    // Write your JavaScript here
+
+    // To select an element by ID use: $w('#elementID')
+
+    // Click 'Preview' to run your code
 });
