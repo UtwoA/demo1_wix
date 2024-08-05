4910e97ce6a7994b9ac50a094acbec2da0344bc1
Алексей <workspace.alexey@gmail.com>
2024-08-05 21:34:45 +0400

    MAP CODE

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -16,3 +16,38 @@ $w.onReady(function () {
         $w('#section7').scrollTo();
     });
 });
+const apiKey = "AIzaSyCFyaPOSCk9Yd1ZpSiA4P93ZTSC3WRdwXA";
+
+const mapElementId = "googleMaps1";
+
+const address = "Ulitsa Sobornaya, Saratov, Saratov Oblast, Russia, 410002";
+
+$w.onReady(function () {
+    let script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${AIzaSyCFyaPOSCk9Yd1ZpSiA4P93ZTSC3WRdwXA}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+});
+
+function initMap() {
+    let geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            let mapOptions = {
+                zoom: 15,
+                center: results[0].geometry.location
+            };
+
+            let map = new google.maps.Map(document.getElementById(mapElementId), mapOptions);
+
+            let marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            console.error('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
